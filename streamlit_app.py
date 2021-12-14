2e285de3df27b340f37220e101eb3796ece85a40
Gustavo Juantorena <gjuantorena@gmail.com>
2021-12-14 11:57:10 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,29 +9,6 @@ import pandas as pd
 
 st.title('Nube de palabras LinkedIn ☁️')
 
-links = {
-    'tmt_link': 'https://mbkhqarelc.cognition.run',
-    'gonogo_link': 'https://mm9wnqlptd.cognition.run',
-    'cdt_link': 'https://do2ptr4gcf.cognition.run',
-    'stopsignal_link': 'https://u6t78zoibs.cognition.run'
-  }
-
-def generar_links_exp(links):
-  """
-  Genera un ID,  lo agrega a cada link definido en el diccionario "links" e imprime el resultado
-  """
-  id = shortuuid.uuid()
-  queries = '?id=' 
-  return (f"""
-  El id es: {id}
-  Los links a los experimentos son los siguientes:
-   * TMT: {links['tmt_link']}{queries}{id}
-   * GO NO GO: {links['gonogo_link']}{queries}{id}
-   * CDT: {links['cdt_link']}{queries}{id}
-   * STOP SIGNAL: {links['stopsignal_link']}{queries}{id}
-  """)
-
-# links = generar_links_exp(links)
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
@@ -48,7 +25,15 @@ if uploaded_file is not None:
      st.write(string_data)
 
      # Can be used wherever a "file-like" object is accepted:
-     dataframe = pd.read_csv(uploaded_file)
+     df_shares = pd.read_csv(uploaded_file)
+        
+texto_de_publicaciones = df_shares['ShareCommentary']
+texto_de_publicaciones = [i for i in texto_de_publicaciones if type(i) == str]
+
+# Obtengo la lista de stopwords (conectores, preposiciones, etc) en espanol gracias a nltk
+
+nltk.download('stopwords')
+stop_words = stopwords.words('spanish')
      
 
 pressed = st.button('Apretame para generar los links')
