40a8f397d396d37a4d28d60a59d4053916c9c49f
Gustavo Juantorena <gjuantorena@gmail.com>
2021-12-14 11:50:43 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,24 @@ def generar_links_exp(links):
 
 # links = generar_links_exp(links)
 
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+     # To read file as bytes:
+     bytes_data = uploaded_file.getvalue()
+     st.write(bytes_data)
+
+     # To convert to a string based IO:
+     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+     st.write(stringio)
+
+     # To read file as string:
+     string_data = stringio.read()
+     st.write(string_data)
+
+     # Can be used wherever a "file-like" object is accepted:
+     dataframe = pd.read_csv(uploaded_file)
+     st.write(dataframe)
+
 pressed = st.button('Apretame para generar los links')
 if pressed:
    st.write(generar_links_exp(links))
