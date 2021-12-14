877fdce6206e0144df8447290b9531b5891f99b0
Gustavo Juantorena <gjuantorena@gmail.com>
2021-12-14 14:22:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,13 @@ from nltk.corpus import stopwords
 import string  
 
 
+
+st.title('☁️ Nube de palabras LinkedIn ☁️')
+
 st.image(
  "https://cdn-icons-png.flaticon.com/512/174/174857.png",
  width= 60, # Manually Adjust the width of the image as per requirement
 )
-st.title('☁️ Nube de palabras LinkedIn ☁️')
 
 st.subheader('El archivo que te proporciona LinkedIn con la info de lo que compartiste se llama "Shares.csv"')
 st.markdown("*Podés ver las instrucciones para obtener tus datos de LinkedIn [entrando acá](https://gist.github.com/GEJ1/68a7525f6e38a074f1474db3e0f894d6)*")
