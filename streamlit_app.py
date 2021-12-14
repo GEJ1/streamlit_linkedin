a178a95ee9d0b95c66e1998f300acfc65fcaf192
Gustavo Juantorena <gjuantorena@gmail.com>
2021-12-14 17:30:39 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,17 +130,18 @@ if __name__ == "__main__":
   st.title('☁️ Nube de palabras LinkedIn ☁️')
 
   st.subheader('Subí el archivo que te proporciona LinkedIn con la info de lo que compartiste (se llama "Shares.csv")')
+
+
+  
+
+  st.markdown("### Presioná **Browse files** y seleccioná tu archivo *Shares.csv*")
+  st.markdown("*Podés ver las instrucciones para obtener tus datos de LinkedIn [entrando acá](https://gist.github.com/GEJ1/68a7525f6e38a074f1474db3e0f894d6)*")
   st.markdown("Si queres ver un ejemplo sin usar tus datos presioná el siguiente botón")      
   pressed = st.button('Ver ejemplo')
 
   # Cargamos template
   if pressed:
      generar_nube_de_palabras('template')
-
-  
-
-  st.markdown("### Presioná **Browse files** y seleccioná tu archivo *Shares.csv*")
-  st.markdown("*Podés ver las instrucciones para obtener tus datos de LinkedIn [entrando acá](https://gist.github.com/GEJ1/68a7525f6e38a074f1474db3e0f894d6)*")
 
   uploaded_file = st.file_uploader("Seleccioná el archivo")
 
