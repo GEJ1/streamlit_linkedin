336d6f8ae5113e24a1f61904ef1ad805ba5fec1d
Gustavo Juantorena <gjuantorena@gmail.com>
2021-12-14 12:02:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Importamos 
+import streamlit as st
 import pandas as pd
 import numpy as np
 
@@ -39,9 +40,70 @@ texto_de_publicaciones = df_shares['ShareCommentary']
 texto_de_publicaciones = [i for i in texto_de_publicaciones if type(i) == str]
 
 # Obtengo la lista de stopwords (conectores, preposiciones, etc) en espanol gracias a nltk
-
 nltk.download('stopwords')
 stop_words = stopwords.words('spanish')
+
+# Uso set para borrar repetidos
+texto = [i for i in set(texto_de_publicaciones) if type(i) == str]
+
+texto = ''.join(texto)
+
+def limpiar_puntuacion_stopwords(texto):
+  """
+  Funcion para limpiar el string
+
+  #Modificado de la siguiente fuente: https://antonio-fernandez-troyano.medium.com/nube-de-palabras-word-cloud-con-python-a-partir-de-varias-webs-111e94220822
+
+  Parameters 
+  ---------------
+  texto (str)       -> Texto a limpiar
+
+  Returns
+  ---------------
+  texto_limpio (str) -> Texto limpio luego de sacarle signos de puntuacion y stopwords
+
+  """
+  puntuacion = []
+  for s in string.punctuation:
+      puntuacion.append(str(s))
+  sp_puntuacion = ["¿", "¡", "“", "”", "…", ":", "–", "»", "«"]    
+
+  puntuacion += sp_puntuacion
+
+  #Reemplazamos signos de puntuación por "":
+  for p in puntuacion:
+      texto_limpio = texto.lower().replace(p,"")
+      
+  for p in puntuacion:
+      texto_limpio = texto_limpio.replace(p,"")
+
+  #Reemplazamos stop_words por "":    
+  for stop in stop_words:
+      texto_limpio_lista = texto_limpio.split()
+      texto_limpio_lista = [i.strip() for i in texto_limpio_lista]
+      try:
+          while stop in texto_limpio_lista: texto_limpio_lista.remove(stop)
+      except:
+          print("Error")
+          pass
+      texto_limpio= " ".join(texto_limpio_lista)
+
+  return texto_limpio
+
+# Limpiamos
+clean_texto = limpiar_puntuacion_stopwords(texto)
+
+# Hacemos el wordcloud
+word_cloud = WordCloud(height=800, width=800, background_color='white',max_words=100, min_font_size=5).generate(clean_texto)
+
+# word_cloud.to_file("./img/ejemplo_sencillo.png") #Guardamos la imagen generada
+
+plt.figure(figsize=(10,8))
+plt.imshow(word_cloud)
+plt.axis('off')
+plt.tight_layout(pad=0)
+plt.show()
+word_cloud.to_file('wordcloud.png')
      
 
 pressed = st.button('Apretame para generar los links')
