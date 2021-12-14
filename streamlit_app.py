1a5b1bcd4d42b194b945b60fac318ddcccc545a4
Gustavo Juantorena <gjuantorena@gmail.com>
2021-12-14 12:39:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ if uploaded_file is not None:
 #      fig.tight_layout(pad=0)
      
      # Sacamos los ticks de los ejes 
-     ax[0].axis('off')
+     ax.axis('off')
 
      
      ax.imshow(word_cloud)
